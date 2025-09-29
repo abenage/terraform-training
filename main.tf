518c73c8936940e777aade816e74caa4e9ab581c
Aaron Benage <abenage@akamai.com>
2025-09-29 12:09:44 -0600

    Formatting, commenting out code that doesn't work.

--- main.tf
+++ main.tf
@@ -1,103 +1,47 @@
-# data "akamai_group" "my_group_id" {
-#   group_name  = "Akamai Technologies - Assets-1-3CV382"
-#   contract_id = "1-3CV382"
+data "akamai_group" "my_group_id" {
+  group_name  = "Akamai Technologies - Assets-1-3CV382"
+  contract_id = "1-3CV382"
 
-# }
+}
 
-# output "my_group_id" {
-#   value = data.akamai_group.my_group_id.id
-# }
+output "my_group_id" {
+  value = data.akamai_group.my_group_id.id
+}
 
+/////////////
 
+data "akamai_contract" "my_contract" {
+  group_name  = "Akamai Technologies - Assets-1-3CV382"
+}
 
-
-
-
-
-# // Get one
-# data "akamai_contract" "my_contract" {
-#   group_name  = "Akamai Technologies - Assets-1-3CV382"
-# }
-
-# output "my_contract" {
-#     value = data.akamai_contract.my_contract
-# }
+output "my_contract" {
+    value = data.akamai_contract.my_contract
+}
 
 
+/////////////
 
 
+data "akamai_properties" "my_properties" {
+  group_id    = "grp_18385"
+  contract_id = "1-3CV382"
+}
+
+output "my_properties" {
+  value = data.akamai_properties.my_properties
+}
 
 
-
-
-
-
-# data "akamai_properties" "my_properties" {
-#   group_id    = "grp_18385"
-#   contract_id = "1-3CV382"
-# }
-
-# output "my_properties" {
-#   value = data.akamai_properties.my_properties
-# }
+/////////////
 
 
 
+data "akamai_appsec_configuration" "all-configurations" {
+}
 
-
-
-
-
-# data "akamai_property" "my_property" {
-#   property_id = "prp_556622"
-#   name= "AMTest"
-# }
-
-
-# output "my_property" {
-#   value = data.akamai_property.my_property
-# }
-
-
-
-
-
-
-
-
-
-
-
-
-# data "akamai_appsec_configuration" "all-configurations" {
-# }
-
-# output "configuration-list" {
-#   value = data.akamai_appsec_configuration.all-configurations.output_text
-# }
-
-
-
-
-
-
-
-
-
-
-
-# data "akamai_appsec_configuration" "my_config" {
-# }
-
-# output "config_id" {
-#   value = data.akamai_appsec_configuration.my_config.config_id
-# }
-
-
-
-
-
-
+output "configuration-list" {
+  value = data.akamai_appsec_configuration.all-configurations.output_text
+}
 
 
 # #taken from docs, but contract is not valid with group name.
@@ -115,4 +59,22 @@
 
 # output "configuration_list" {
 #   value = data.akamai_appsec_configuration.configurations.output_text
+# }
+
+
+# data "akamai_appsec_configuration" "my_config" {
+# }
+
+# output "config_id" {
+#   value = data.akamai_appsec_configuration.my_config.config_id
+# }
+
+# Doesn't work. 
+# data "akamai_property" "my_property" {
+#   name= "AMTest"
+# }
+
+
+# output "my_property" {
+#   value = data.akamai_property.my_property
 # }
